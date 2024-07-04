6605f5f6af5384b00c61281d465cb6b7aea275c2
Leonardo Montini <lion.48m@gmail.com>
2024-07-04 23:11:53 +0200

    feat: dashboard link in the homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,24 @@
+import { createClient } from "@/lib/supabase/server";
+import Link from "next/link";
+
 export default async function Home() {
-  return <main>Home</main>;
+  const {
+    data: { user },
+  } = await createClient().auth.getUser();
+
+  return (
+    <main>
+      <h2 className="text-xl font-medium">Home</h2>
+      {user ? (
+        <>
+          <p>Welcome, {user.user_metadata.full_name}!</p>
+          <Link href="/dashboard" className="text-blue-600 hover:underline">
+            Go to dashboard
+          </Link>
+        </>
+      ) : (
+        <p>You are not logged in.</p>
+      )}
+    </main>
+  );
 }
