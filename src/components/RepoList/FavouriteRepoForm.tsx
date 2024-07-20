1db9d1677a9908c6be0274d98d1df1a58d688668
Leonardo Montini <lion.48m@gmail.com>
2024-07-20 13:44:36 +0200

    feat: optimistic favourite

--- src/components/RepoList/FavouriteRepoForm.tsx
+++ src/components/RepoList/FavouriteRepoForm.tsx
@@ -1,19 +1,33 @@
+"use client";
+
 import { Star } from "lucide-react";
 import { Button } from "../ui/button";
 import { toggleFavourite } from "./actions";
+import { useOptimistic } from "react";
 
 type Props = { repoId: string; isFavorite: boolean };
+
 export function FavouriteRepoForm({ repoId, isFavorite }: Props) {
+  const [optimisticIsFavourite, optimisticToggleFavourite] = useOptimistic<
+    boolean,
+    boolean
+  >(isFavorite, (_, newIsFavourite) => newIsFavourite);
+
   return (
-    <form action={toggleFavourite}>
+    <form
+      action={async (formData: FormData) => {
+        optimisticToggleFavourite(!optimisticIsFavourite);
+        toggleFavourite(formData);
+      }}
+    >
       <input type="hidden" name="id" value={repoId} />
       <input
         type="hidden"
         name="isFavorite"
-        value={isFavorite ? "true" : "false"}
+        value={optimisticIsFavourite ? "true" : "false"}
       />
       <Button size={"icon"}>
-        <Star className={isFavorite ? "text-yellow-400" : ""} />
+        <Star className={optimisticIsFavourite ? "text-yellow-400" : ""} />
       </Button>
     </form>
   );
