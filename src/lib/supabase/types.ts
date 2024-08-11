9895fc99a8104f289a052dc794addb4e38e195b2
Leonardo Montini <lion.48m@gmail.com>
2024-08-11 15:25:21 +0200

    chore: setup gha

--- src/lib/supabase/types.ts
+++ src/lib/supabase/types.ts
@@ -1,5 +1,8 @@
-import { Database, Tables } from "./types.gen";
+import { SupabaseClient } from "@supabase/supabase-js"
+import { Database, Tables } from "./types.gen"
 
-export type Task = Tables<"tasks">;
-export type TaskInsert = Database["public"]["Tables"]["tasks"]["Insert"];
-export type TaskUpdate = Database["public"]["Tables"]["tasks"]["Update"];
+export type DatabaseClient = SupabaseClient<Database>
+
+export type Task = Tables<"tasks">
+export type TaskInsert = Database["public"]["Tables"]["tasks"]["Insert"]
+export type TaskUpdate = Database["public"]["Tables"]["tasks"]["Update"]
