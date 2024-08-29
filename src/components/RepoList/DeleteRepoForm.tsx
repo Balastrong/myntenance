7c483fa0b0754fccb2275f5a36c056b75932b5ae
Zeeshan Mukhtar <zeshanmukhtar878@gmail.com>
2024-08-29 21:26:54 +0500

    🗑️ feat(delete-repo-form): Add loading spinner and toaster feedback
    - Implemented a loading spinner to indicate deletion in progress.
    - Added a toaster notification to confirm successful deletion with a positive message.
    - Improved user experience by providing visual feedback during the deletion process.

--- src/components/RepoList/DeleteRepoForm.tsx
+++ src/components/RepoList/DeleteRepoForm.tsx
@@ -1,12 +1,35 @@
+"use client"
 import { handleDelete } from "./actions"
 import { Button } from "../ui/button"
+import { Loader2 } from "lucide-react"
+import { useState } from "react"
+import { toast } from "sonner"
 
 export default function DeleteRepoForm({ repoId }: { repoId: string }) {
+  const [loading, setLoading] = useState(false)
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    setLoading(true)
+
+    try {
+      const formData = new FormData(event.currentTarget)
+      await handleDelete(formData)
+
+      toast.success("Repository deleted successfully.")
+    } catch (error) {
+      console.error("Failed to delete the repository:", error)
+      alert("Failed to delete the repository.")
+    } finally {
+      setLoading(false)
+    }
+  }
+
   return (
-    <form action={handleDelete}>
+    <form onSubmit={handleSubmit}>
       <input type="hidden" name="id" value={repoId} />
-      <Button size={"sm"} variant={"destructive"}>
-        Delete
+      <Button size={"sm"} variant={"destructive"} disabled={loading}>
+        {loading ? <Loader2 className="mr-2 h-4 w-4 animate-spin" /> : "Delete"}
       </Button>
     </form>
   )
