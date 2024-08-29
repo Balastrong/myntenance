4a8d23305face6c2570a86fdca6ee28ba75718b6
Zeeshan Mukhtar <zeshanmukhtar878@gmail.com>
2024-08-29 21:33:49 +0500

    ✨ feat(delete-repo-form): Add confirmation dialog before deletion
    - Implemented a confirmation dialog to prompt the user before deleting a repository.
    - Improved the deletion process by providing an additional safety measure.

--- src/components/RepoList/DeleteRepoForm.tsx
+++ src/components/RepoList/DeleteRepoForm.tsx
@@ -4,16 +4,26 @@ import { Button } from "../ui/button"
 import { Loader2 } from "lucide-react"
 import { useState } from "react"
 import { toast } from "sonner"
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
 
 export default function DeleteRepoForm({ repoId }: { repoId: string }) {
   const [loading, setLoading] = useState(false)
 
-  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault()
+  const handleSubmit = async () => {
     setLoading(true)
-
     try {
-      const formData = new FormData(event.currentTarget)
+      const formData = new FormData()
+      formData.append("id", repoId)
       await handleDelete(formData)
 
       toast.success("Repository deleted successfully.")
@@ -26,11 +36,29 @@ export default function DeleteRepoForm({ repoId }: { repoId: string }) {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input type="hidden" name="id" value={repoId} />
-      <Button size={"sm"} variant={"destructive"} disabled={loading}>
-        {loading ? <Loader2 className="mr-2 h-4 w-4 animate-spin" /> : "Delete"}
-      </Button>
-    </form>
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button size={"sm"} variant={"destructive"} disabled={loading}>
+          {loading ? (
+            <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+          ) : (
+            "Delete"
+          )}
+        </Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This action cannot be undone. This will permanently delete your
+            repository and remove all associated data.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction onClick={handleSubmit}>Continue</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
   )
 }
