496c475336eb2edc7af05432c1976f76a56bad4a
Leonardo Montini <lion.48m@gmail.com>
2024-08-31 14:40:42 +0200

    feat: two columns layout

--- src/app/[userSlug]/page.tsx
+++ src/app/[userSlug]/page.tsx
@@ -17,22 +17,28 @@ export default async function UserPublicProfile({
   const { data: publicProjects } = await getUserPublicProjects(userData.user)
 
   return (
-    <main className="flex h-full flex-col gap-4">
-      <h2 className="text-xl font-medium">Profile: {userSlug}</h2>
-      <Avatar>
-        <AvatarImage src={`https://github.com/${userSlug}.png`} />
-        <AvatarFallback>{userSlug[0]}</AvatarFallback>
-      </Avatar>
-      <div>Full name: {userData?.fullName}</div>
-      <div>
-        Public Projects:
+    <main className="flex flex-col gap-4 lg:flex-row">
+      <div className="flex min-w-64 flex-col gap-4">
+        <Avatar className="mx-auto size-48">
+          <AvatarImage src={`https://github.com/${userSlug}.png`} />
+          <AvatarFallback>{userSlug[0]}</AvatarFallback>
+        </Avatar>
+        <div>
+          <h2 className="text-2xl font-medium">{userData?.fullName}</h2>
+          <h3 className="text-xl">{userSlug}</h3>
+        </div>
+        <div>
+          <p>Projects: {publicProjects?.length ?? 0}</p>
+        </div>
+      </div>
+      <div className="flex flex-col items-center gap-4">
+        <h2 className="text-2xl font-medium">Public Projects</h2>
         <div className="flex flex-col items-center gap-4">
           {(publicProjects ?? []).map((project) => (
             <PublicProjectCard
               key={project.id}
               userLogin={userSlug}
-              repoLogin={project.ownerLogin}
-              repoName={project.name}
+              project={project}
             />
           ))}
         </div>
--- src/components/PublicProjectCard.tsx
+++ src/components/PublicProjectCard.tsx
@@ -1,55 +1,79 @@
 import { getUserRepoStats } from "@/app/api/github/actions"
-import { Card, CardContent, CardHeader, CardTitle } from "./ui/card"
 import { getServerOctokit } from "@/lib/github/server"
+import { Activity } from "react-activity-calendar"
 import { ActivityCalendar } from "./ActivityCalendar"
+import { Card, CardContent, CardHeader, CardTitle } from "./ui/card"
+import Image from "next/image"
+import { RepoWithTasks } from "./RepoList/RepoCards"
+import { Database, Tables } from "@/lib/supabase/types.gen"
 
 type Props = {
   userLogin: string
-  repoLogin: string
-  repoName: string
+  project: Tables<"projects">
 }
 
-export async function PublicProjectCard({
-  userLogin,
-  repoLogin,
-  repoName,
-}: Props) {
+export async function PublicProjectCard({ userLogin, project }: Props) {
   const { data: commits } = await getUserRepoStats(
     getServerOctokit(),
     userLogin,
-    repoLogin,
-    repoName,
+    project.ownerLogin,
+    project.name,
   )
 
-  const dataMap = new Map<
-    string,
-    {
-      date: string
-      count: number
-      level: number
-    }
-  >()
+  const totalCommits = commits.length
 
+  const data = computeActivityCalendarData(commits)
+
+  return (
+    <Card
+      style={{
+        order: -totalCommits,
+      }}
+    >
+      <CardHeader>
+        <CardTitle className="flex items-center gap-2">
+          <Image
+            width={100}
+            height={100}
+            src={`https://github.com/${project.ownerLogin}.png?size=80`}
+            alt={project.ownerLogin}
+            className={`size-10 bg-gray-300 ${
+              project.ownerType === "User" ? "rounded-full" : "rounded-md"
+            }`}
+          />
+          {project.ownerLogin}/{project.name}
+        </CardTitle>
+      </CardHeader>
+      <CardContent>
+        <ActivityCalendar data={data} />
+      </CardContent>
+    </Card>
+  )
+}
+
+function computeActivityCalendarData(
+  commits: Awaited<ReturnType<typeof getUserRepoStats>>["data"],
+) {
+  let max = 0
+
+  const dataMap = new Map<string, Activity>()
   const date = new Date()
 
   for (let i = 0; i < 365; i++) {
     date.setDate(date.getDate() - 1)
-    dataMap.set(date.toISOString().split("T")[0], {
-      date: date.toISOString().split("T")[0],
+    const dateString = date.toISOString().split("T")[0]
+    dataMap.set(dateString, {
+      date: dateString,
       count: 0,
       level: 0,
     })
   }
-
-  let max = 0
-  let totalCommits = 0
 
   commits.forEach(({ commit }) => {
     const date = commit.author?.date?.split("T")[0]
     if (!date) return
     const data = dataMap.get(date)
     if (data) {
-      totalCommits++
       const count = data.count + 1
       max = Math.max(max, count)
       dataMap.set(date, {
@@ -74,20 +98,5 @@ export async function PublicProjectCard({
     })
     .toReversed()
 
-  return (
-    <Card
-      style={{
-        order: -totalCommits,
-      }}
-    >
-      <CardHeader>
-        <CardTitle>
-          {repoLogin}/{repoName}
-        </CardTitle>
-      </CardHeader>
-      <CardContent>
-        <ActivityCalendar data={data} />
-      </CardContent>
-    </Card>
-  )
+  return data
 }
