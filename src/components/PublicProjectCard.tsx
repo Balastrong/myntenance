10d00d0d502e966142cdcbb4125130dcec4e57ef
Philipe A. Marques <34419519+xRiku@users.noreply.github.com>
2024-12-10 06:13:09 -0300

    feat: add last commit date to projects (#64)
    * feat: add last commit date to projects
    
    * refactor: remove explicit locale
    
    Co-authored-by: Leonardo Montini <lion.48m@gmail.com>
    
    * fix: revert createAdminClient change
    
    ---------
    
    Co-authored-by: Leonardo Montini <lion.48m@gmail.com>

--- src/components/PublicProjectCard.tsx
+++ src/components/PublicProjectCard.tsx
@@ -7,6 +7,7 @@ import { use, useMemo } from "react"
 import { Activity } from "react-activity-calendar"
 import { ActivityCalendar } from "./ActivityCalendar"
 import { Card, CardContent, CardHeader, CardTitle } from "./ui/card"
+import { History } from "lucide-react"
 
 type Props = {
   project: Tables<"projects">
@@ -31,25 +32,34 @@ export function PublicProjectCard({ project, activityPromise }: Props) {
   return (
     <Card className="w-full max-w-fit" style={{ order }}>
       <CardHeader>
-        <CardTitle className="flex items-center gap-2">
-          <Image
-            width={40}
-            height={40}
-            src={`https://github.com/${project.ownerLogin}.png?size=80`}
-            alt={project.ownerLogin}
-            className={`size-10 bg-gray-300 ${
-              project.ownerType === "User" ? "rounded-full" : "rounded-md"
-            }`}
-          />
-          <a
-            href={`https://github.com/${project.ownerLogin}/${project.name}`}
-            target="_blank"
-            className="text-nowrap"
-          >
-            <span>{project.ownerLogin}/</span>
-            <wbr />
-            <span>{project.name}</span>
-          </a>
+        <CardTitle className="flex items-center justify-between">
+          <div className="flex items-center gap-2">
+            <Image
+              width={40}
+              height={40}
+              src={`https://github.com/${project.ownerLogin}.png?size=80`}
+              alt={project.ownerLogin}
+              className={`size-10 bg-gray-300 ${
+                project.ownerType === "User" ? "rounded-full" : "rounded-md"
+              }`}
+            />
+            <a
+              href={`https://github.com/${project.ownerLogin}/${project.name}`}
+              target="_blank"
+              className="text-nowrap"
+            >
+              <span>{project.ownerLogin}/</span>
+              <wbr />
+              <span>{project.name}</span>
+            </a>
+          </div>
+
+          {lastCommit && (
+            <span className="flex items-center gap-1 text-xs font-medium">
+              <History className="h-4 w-4" /> Last commit:{" "}
+              {new Date(lastCommit).toLocaleDateString()}
+            </span>
+          )}
         </CardTitle>
       </CardHeader>
       <CardContent>
